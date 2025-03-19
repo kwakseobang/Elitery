7028f991114afbb0f55e5185d7649714a59daaa5
kwakseobang <iii6602@naver.com>
2025-03-19 12:53:44 +0900

    Refactor[#9]: repository structure update

--- src/main/java/com/kwakmunsu/elitery/member/infrastruction/MemberAppender.java
+++ src/main/java/com/kwakmunsu/elitery/member/infrastruction/MemberAppender.java
@@ -3,10 +3,30 @@ package com.kwakmunsu.elitery.member.infrastruction;
 
 import com.kwakmunsu.elitery.auth.oauth2.userinfo.OAuth2UserInfo;
 import com.kwakmunsu.elitery.member.entity.Member;
+import com.kwakmunsu.elitery.member.entity.Role;
 import com.kwakmunsu.elitery.member.entity.SocialType;
+import com.kwakmunsu.elitery.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
-public interface MemberAppender {
+@Component
+@RequiredArgsConstructor
+public class MemberAppender {
 
-    Member create(OAuth2UserInfo userInfo, SocialType socialType);
+    private final MemberRepository memberRepository;
+
+    public Member create(OAuth2UserInfo userInfo, SocialType socialType) {
+        Member member = Member.builder()
+            .email(userInfo.getProvider() + ": " + userInfo.getEmail()) // Naver: iii6602@naver.com
+            .nickname(userInfo.getName())
+            .imageUrl(null) // 명시적으로 하기 위해서
+            .role(Role.MEMBER)
+            .socialType(socialType)
+            .socialId(userInfo.getSocialId())
+            .introduction("")
+            .build();
+
+        return memberRepository.append(member);
+    }
 
 }
--- src/main/java/com/kwakmunsu/elitery/member/infrastruction/MemberReader.java
+++ src/main/java/com/kwakmunsu/elitery/member/infrastruction/MemberReader.java
@@ -1,10 +1,20 @@
 package com.kwakmunsu.elitery.member.infrastruction;
 
 import com.kwakmunsu.elitery.member.entity.Member;
+import com.kwakmunsu.elitery.member.repository.MemberRepository;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
-public interface MemberReader {
+@Component
+@RequiredArgsConstructor
 
-    Optional<Member> getMember(String socialId);
+public class MemberReader {
+
+    private final MemberRepository memberRepository;
+
+    public Optional<Member> read(String socialId) {
+        return memberRepository.read(socialId);
+    }
 
 }
